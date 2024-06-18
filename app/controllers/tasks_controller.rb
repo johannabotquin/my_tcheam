9da81b776b561aa2e6cd9281a54db95178b643a1
Zak Djermani <zak.djermani@gmail.com>
2024-06-18 14:20:05 +0200

    tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task), status: see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :category, :reccurence, :points, :deadline, :achieved, :priority_tag)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
