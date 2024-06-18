a55e1370f32dbf6cfb9a822a3e8fd70c340d0453
Zak Djermani <zak.djermani@gmail.com>
2024-06-18 12:36:00 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Team.destroy_all
+User.destroy_all
+
+puts "creating teams..."
+
+team = Team.create(
+  name: "The Avengers",
+  score: 0
+)
+puts "end teams"
+puts "creating users..."
+
+User.create(
+  first_name: "Johanna",
+  last_name: "Botquin",
+  pseudo: "Jojo",
+  email: "johanna@gmail.com",
+  password: "angryjojo",
+  avatar: "https://pictory.ai/wp-content/uploads/2022/03/pictory_500.png",
+  team_id: team.id
+)
+
+User.create(
+  first_name: "Ragheed",
+  last_name: "Sleimani",
+  pseudo: "Rara",
+  email: "ragheed.sleimani@gmail.com",
+  password: "zenragheed",
+  avatar: "https://pictory.ai/wp-content/uploads/2022/03/pictory_500.png",
+  team_id: team.id
+)
+
+puts "end users"
