a810b6d49eca13c9a2142e2efdf5ba23663e7d86
Johanna Botquin <johannabotquin@icloud.com>
2024-06-20 11:27:37 +0200

    change column points task in string + début score

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
 
   def show
+    @user = current_user
+    @tasks = @user.tasks
   end
 
   private
---
+++ db/migrate/20240620092532_add_column_points_from_tasks.rb
@@ -0,0 +1,4 @@
+class AddColumnPointsFromTasks < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
