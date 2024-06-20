3b8c41ed44c1c3cf2998c474984b0e98c2e7a410
laura cabos <lauracabos@gmail.com>
2024-06-20 16:32:13 +0200

    modal edit task ongoing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -59,7 +59,7 @@ class TasksController < ApplicationController
 
   def update
     @task.update(task_params)
-    redirect_to task_path(@task)
+    redirect_to tasks_path
   end
 
   def destroy
@@ -70,7 +70,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :category, :reccurence, :points, :deadline, :achieved, :priority_tag)
+    params.require(:task).permit(:name, :description, :category, :reccurence, :points, :deadline, :achieved, :priority)
   end
 
   def set_task
