b478c0a0c0f7cfdfa3bc792e8bf00f271b4860a9
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <Raghid.sleiman@kedgebs.com>
2024-06-24 16:09:10 +0200

    added select new reward button in reward update

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,7 +1,22 @@
 class RewardsController < ApplicationController
+  # def index
+  #   # @team = Team.find(params[:team_id])
+  #   # @rewards = @team.rewards
+
+  # end
+
+  before_action :set_team
+
   def index
-    @team = Team.find(params[:team_id])
     @rewards = @team.rewards
+    @selected_reward = @team.rewards.find_by(selected: true)
+  end
+
+  def select
+    @reward = @team.rewards.find(params[:id])
+    @team.rewards.update_all(selected: false)
+    @reward.update(selected: true)
+    redirect_to team_rewards_path(@team), notice: 'La récompense a été mise en avant.'
   end
 
   def new
@@ -14,6 +29,7 @@ class RewardsController < ApplicationController
     @reward = Reward.new(reward_params)
     @reward.team_id = current_user.team_id
     @reward.user_id = current_user.id
+
     if @reward.save
       redirect_to team_rewards_path(@team), notice: 'La recompense est créée.'
     else
@@ -48,4 +64,13 @@ class RewardsController < ApplicationController
   def reward_params
     params.require(:reward).permit(:name, :goal)
   end
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:name, :goal, :selected)
+  end
+
 end
