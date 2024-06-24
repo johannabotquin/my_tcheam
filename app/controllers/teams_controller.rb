9400390d0a0c8f3dca7f9808688e0f927d98697b
Zak Djermani <zak.djermani@gmail.com>
2024-06-24 17:12:45 +0200

    undo score dashboard team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,14 +2,9 @@ class TeamsController < ApplicationController
 
   def show
     @team = current_user.team
-    if @reward.nil?
-      @team.update(score: 0)
-    else
-      @team.score = @team.users.sum do |user|
+    @team.score = @team.users.sum do |user|
       user.tasks.where(achieved: true).sum(:points)
-      end
     end
-    @reward = @team.rewards.find { |reward| reward.selected == true }
     @tasks_by_user = @team.users.each_with_object({}) do |user, hash|
       user_tasks = user.tasks
       if params[:filter] && params[:filter][:date].present?
