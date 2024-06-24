2d36b91344965ed71837034a154395757f7d3234
laura cabos <lauracabos@gmail.com>
2024-06-24 18:07:19 +0200

    animate ongoing

--- app/javascript/controllers/dispatch_controller.js
+++ app/javascript/controllers/dispatch_controller.js
@@ -3,17 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dispatch"
 export default class extends Controller {
 
-  static targets = [ "card" ]
+  static targets = [ "card", "faceB" ]
   connect() {
     console.log("Connected to dispatch controller")
   }
 
   dispatch(event) {
-    event.preventDefault()
-    console.log(this.cardTargets)
-    this.cardTargets.forEach((card) => {
-      console.log(card)
-      card.classList.remove("d-none")
-    })
-  }
+    event.preventDefault();
+    console.log(this.cardTargets);
+    this.cardTargets.forEach((card, index) => {
+      // Utilise setTimeout pour décaler l'ajout de la classe
+      setTimeout(() => {
+        console.log(card);
+        card.classList.add("flip-this-card");
+      }, index * 500); // Décale de 500ms pour chaque carte
+    });
+}
 }
