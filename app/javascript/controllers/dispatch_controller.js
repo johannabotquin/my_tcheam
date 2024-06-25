b7ebe72c731460576c96b8b33a3e1f490073a47a
laura cabos <lauracabos@gmail.com>
2024-06-25 10:21:43 +0200

    wheel works

--- app/javascript/controllers/dispatch_controller.js
+++ app/javascript/controllers/dispatch_controller.js
@@ -3,20 +3,27 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="dispatch"
 export default class extends Controller {
 
-  static targets = [ "card", "faceB" ]
+  static targets = [ "card", "avatar" ]
   connect() {
     console.log("Connected to dispatch controller")
   }
 
   dispatch(event) {
-    event.preventDefault();
-    console.log(this.cardTargets);
-    this.cardTargets.forEach((card, index) => {
+     this.cardTargets.forEach((card, index) => {
       // Utilise setTimeout pour décaler l'ajout de la classe
       setTimeout(() => {
-        console.log(card);
         card.classList.add("flip-this-card");
       }, index * 500); // Décale de 500ms pour chaque carte
     });
-}
+
+    setTimeout(() => {
+      this.avatarTargets.forEach((avatar, index) => {
+        // Utilise setTimeout pour décaler l'ajout de la classe
+            setTimeout(() => {
+              avatar.classList.remove("d-none");
+            }, index * 500); // Décale de 500ms pour chaque carte
+          })
+    }, 500);
+  }
+
 }
