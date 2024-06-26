f8d6933318a8b1426cde7824cb7a22d7201d224a
laura cabos <lauracabos@gmail.com>
2024-06-26 17:04:20 +0200

    fixed team_id null

--- db/migrate/20240626142400_add_reference_user_to_chatroom.rb
+++ db/migrate/20240626142400_add_reference_user_to_chatroom.rb
@@ -1,5 +1,5 @@
 class AddReferenceUserToChatroom < ActiveRecord::Migration[7.1]
   def change
-    add_reference :chatrooms, :team, null: false, foreign_key: true
+    add_reference :chatrooms, :team, foreign_key: true
   end
 end
